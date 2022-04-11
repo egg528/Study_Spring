ffe82d3106d2c5be8da8c6d386a6ae64f8218474
권우석 <egg528@hanyang.ac.kr>
2022-04-11 20:50:02 +0900

    Spring DI (2)

---
+++ Chap3 스프링 DI/src/main/java/DuplicateMemberException.java
@@ -0,0 +1,7 @@
+public class DuplicateMemberException extends RuntimeException{
+
+    public DuplicateMemberException(String message){
+        // super는 부모 클래스의 다른 생성자를 호출할 떄 사용한다.
+        super(message);
+    }
+}
---
+++ Chap3 스프링 DI/src/main/java/Member.java
@@ -0,0 +1,29 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Getter @Setter
+public class Member {
+
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private LocalDateTime registerDateTime;
+
+    public Member(String email, String password, String name, LocalDateTime registerDateTime){
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.registerDateTime = registerDateTime;
+    }
+
+    public void changePassword(String oldPassword, String newPassword){
+        if(!password.equals(oldPassword))
+            throw new WrongIdPasswordException();
+        this.password = newPassword;
+    }
+}
