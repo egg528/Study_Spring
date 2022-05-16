6dd55ed0510bedae9c13afdb71b201fadf1228f0
egg528 <egg528@hanyang.ac.kr>
2022-05-17 00:09:44 +0900

    MVC1 controller 객체 파라미터 추가

--- Chap11 요청 매핑, 커맨드 객체, 리다이렉트, 폼 태그, 모델/MVC/src/main/java/com/example/mvc/controller/RegisterController.java
+++ Chap11 요청 매핑, 커맨드 객체, 리다이렉트, 폼 태그, 모델/MVC/src/main/java/com/example/mvc/controller/RegisterController.java
@@ -5,6 +5,7 @@ import com.example.mvc.spring.MemberRegisterService;
 import com.example.mvc.spring.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +24,10 @@ public class RegisterController {
     }
 
     @PostMapping("/step2")
-    public String handleStep2(@RequestParam(value = "agree", defaultValue = "false") Boolean agree){
+    public String handleStep2(@RequestParam(value = "agree", defaultValue = "false") Boolean agree, Model model){
         if(!agree) return "register/step1";
 
+        model.addAttribute("registerRequest", new RegisterRequest());
         return "register/step2";
     }
 
