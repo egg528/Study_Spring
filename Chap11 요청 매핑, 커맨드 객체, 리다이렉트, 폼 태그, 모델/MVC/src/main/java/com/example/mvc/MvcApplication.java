59e92869c9a294613977a6af36126ab302bdc7ba
egg528 <egg528@hanyang.ac.kr>
2022-05-17 22:49:26 +0900

    MVC1 에러 테스트

--- Chap11 요청 매핑, 커맨드 객체, 리다이렉트, 폼 태그, 모델/MVC/src/main/java/com/example/mvc/MvcApplication.java
+++ Chap11 요청 매핑, 커맨드 객체, 리다이렉트, 폼 태그, 모델/MVC/src/main/java/com/example/mvc/MvcApplication.java
@@ -3,12 +3,19 @@ package com.example.mvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MvcApplication {
+public class MvcApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/main").setViewName("main");
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(MvcApplication.class, args);
     }
-
 }
